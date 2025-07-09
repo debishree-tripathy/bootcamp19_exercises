23a09e0eb890cddeb914e0a578a9ab768f625753
debishree-tripathy <debishree.tripathy@dhdigital.co.in>
2025-07-09 21:43:53 +0530

    Update file.sh

--- Advanced Linux and Shell Scripting 11-02-2019/file.sh
+++ Advanced Linux and Shell Scripting 11-02-2019/file.sh
@@ -1,8 +1,9 @@
-#bin/bash
+#!/bin/bash
 echo $1
 echo $2
 echo $3
-export var1="tothenew"
-echo ${var1}
-bash file.sh
 
+export var1="tothenew"
+echo "${var1}"
+
+# Removed the recursive bash call to prevent infinite loop
